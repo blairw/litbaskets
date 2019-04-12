20285dea694c35119adb6df5e97307ff8ad16b04
Blair Wang <hello@blair.wang>
2019-04-12 16:14:18 +1000

    [feat] Allow www-beta

--- api/cors.php
+++ api/cors.php
@@ -2,7 +2,10 @@
 	// CORS
 	if(isset($_SERVER['HTTP_ORIGIN'])) {
 		$http_origin = $_SERVER['HTTP_ORIGIN'];
-		if ($http_origin == "https://litbaskets.io" || $http_origin == "https://www.litbaskets.io")
+		if ($http_origin == "https://litbaskets.io"
+			|| $http_origin == "https://www.litbaskets.io"
+			|| $http_origin == "https://www-beta.litbaskets.io"
+		)
 		{  
 			header("Access-Control-Allow-Origin: $http_origin");
 		}
