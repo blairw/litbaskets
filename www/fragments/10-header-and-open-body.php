6bca4e28c1bd52b2049bfccf4cc81160ea294604
Blair Wang <hello@blair.wang>
2019-04-24 15:47:43 +1000

    [chore] litbasketsRevision=27

--- www/fragments/10-header-and-open-body.php
+++ www/fragments/10-header-and-open-body.php
@@ -2,7 +2,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="node_modules/patternfly/dist/css/patternfly.min.css">
 	<link rel="stylesheet" type="text/css" href="node_modules/patternfly/dist/css/patternfly-additions.min.css">
-	<link rel="stylesheet" type="text/css" href="extras/styles.css?litbasketsRevision=27" />
+	<link rel="stylesheet" type="text/css" href="extras/styles.css?litbasketsRevision=28" />
 	
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -11,12 +11,12 @@
 	<script src="node_modules/patternfly-bootstrap-combobox/js/bootstrap-combobox.js"></script>
 	<script src="node_modules/underscore/underscore-min.js"></script>
 	<script src="node_modules/clipboard/dist/clipboard.min.js"></script>
-	<script src="extras/sidebar-tab-switcher.js?litbasketsRevision=27"></script>
-	<script src="extras/api.js?litbasketsRevision=27"></script>
-	<script src="extras/index.model.js?litbasketsRevision=27"></script>
-	<script src="extras/index.controller.js?litbasketsRevision=27"></script>
-	<script src="extras/index.controller.search.js?litbasketsRevision=27"></script>
-	<script src="extras/index.controller.sources.js?litbasketsRevision=27"></script>
+	<script src="extras/sidebar-tab-switcher.js?litbasketsRevision=28"></script>
+	<script src="extras/api.js?litbasketsRevision=28"></script>
+	<script src="extras/index.model.js?litbasketsRevision=28"></script>
+	<script src="extras/index.controller.js?litbasketsRevision=28"></script>
+	<script src="extras/index.controller.search.js?litbasketsRevision=28"></script>
+	<script src="extras/index.controller.sources.js?litbasketsRevision=28"></script>
 
 	
 	<title>Litbaskets</title>
