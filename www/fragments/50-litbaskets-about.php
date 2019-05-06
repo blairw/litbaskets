f3049d1ef15884c6a8e15c6fb9ea6853431253eb
Blair Wang <hello@blair.wang>
2019-05-06 14:47:36 +1000

    [fix] References synchronised with Paper submitted

--- www/fragments/50-litbaskets-about.php
+++ www/fragments/50-litbaskets-about.php
@@ -6,16 +6,16 @@
 	</p>
 	<h2>Where did the baskets come from?</h2>
 	<p class="litfam_bibliography_entry">
-		ABDC, 2013, <em>ABDC Journal Quality List</em>, <a href="https://abdc.edu.au/research/abdc-journal-list/2013-review/" target="_blank">https://abdc.edu.au/research/abdc-journal-list/2013-review/</a>
+		Association for Information Systems (AIS), 2011, <em>Senior Scholars' Basket of Journals</em>, <a href="https://aisnet.org/general/custom.asp?page=SeniorScholarBasket" target="_blank">https://aisnet.org/general/custom.asp?page=SeniorScholarBasket</a>
 	</p>
 	<p class="litfam_bibliography_entry">
-		AIS Members of the College of Senior Scholars, 2011, <em>Senior Scholars' Basket of Journals</em>, <a href="https://aisnet.org/page/SeniorScholarBasket" target="_blank">https://aisnet.org/page/SeniorScholarBasket</a>
+		Australian Business Deans Council (ABDC), 2016, <em>ABDC Journal Quality List</em>, <a href="https://abdc.edu.au/research/abdc-journal-list/" target="_blank">https://abdc.edu.au/research/abdc-journal-list/</a>
 	</p>
 	<p class="litfam_bibliography_entry">
-		Chan H.C., Guness V., Kim H.-W., 2015, 'A method for identifying journals in a discipline: An application to information systems', <em>Information & Management</em>, vol. 52, doi: <a href="https://dx.doi.org/10.1016/j.im.2014.11.003" target="_blank">10.1016/j.im.2014.11.003</a>
+		Chan H.C., Guness V., Kim H.-W., 2015, 'A method for identifying journals in a discipline: An application to information systems', <em>Information and Management</em>, vol. 52, no. 2, doi: <a href="https://dx.doi.org/10.1016/j.im.2014.11.003" target="_blank">10.1016/j.im.2014.11.003</a>
 	</p>
 	<p class="litfam_bibliography_entry">
-		Chartered Association of Business Schools, 2018, <em>Academic Journal Guide</em>, <a href="https://charteredabs.org/academic-journal-guide-2018/" target="_blank">https://charteredabs.org/academic-journal-guide-2018/</a>
+		Chartered Association of Business Schools (CABS), 2018, <em>Academic Journal Guide</em>, <a href="https://charteredabs.org/academic-journal-guide-2018/" target="_blank">https://charteredabs.org/academic-journal-guide-2018/</a>
 	</p>
 	<p class="litfam_bibliography_entry">
 		Holsapple C.W., 2009, 'A new map for Knowledge dissemination channels', <em>Communications of the ACM</em>, vol. 52, no. 3, doi: <a href="https://dx.doi.org/10.1145/1467247.1467276" target="_blank">10.1145/1467247.1467276</a>
@@ -27,6 +27,6 @@
 		Stewart A., Cotton J.L., & Adya M., 2017, 'Information Systems: A House Divided?', <em>Communications of the Association for Information Systems</em>, vol. 41, doi: <a href="https://dx.doi.org/10.17705/1CAIS.04124" target="_blank">10.17705/1CAIS.04124</a>
 	</p>
 	<p class="litfam_bibliography_entry">
-		Wilcocks L., Whitley E.A., & Avgerou C., 2008, 'The ranking of top IS journals: a perspective from the London School of Economics', <em>European Journal of Information Systems</em>, vol. 17, doi: <a href="https://dx.doi.org/10.1057/ejis.2008.9" target="_blank">10.1057/ejis.2008.9</a>
+		Wilcocks L., Whitley E.A., & Avgerou C., 2008, 'The ranking of top IS journals: a perspective from the London School of Economics', <em>European Journal of Information Systems</em>, vol. 17, no. 2, doi: <a href="https://dx.doi.org/10.1057/ejis.2008.9" target="_blank">10.1057/ejis.2008.9</a>
 	</p>
 </div>
