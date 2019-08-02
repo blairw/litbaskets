f8d4c64f32c465a00badf4faea31d78e55ce546a
Blair Wang <hello@blair.wang>
2019-08-02 09:31:58 +0900

    [chore] litbasketsRevision=39

--- www/fragments/10-header-and-open-body.php
+++ www/fragments/10-header-and-open-body.php
@@ -2,7 +2,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="node_modules/patternfly/dist/css/patternfly.min.css">
 	<link rel="stylesheet" type="text/css" href="node_modules/patternfly/dist/css/patternfly-additions.min.css">
-	<link rel="stylesheet" type="text/css" href="extras/styles.css?litbasketsRevision=38" />
+	<link rel="stylesheet" type="text/css" href="extras/styles.css?litbasketsRevision=39" />
 	
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -12,14 +12,14 @@
 	<script src="node_modules/patternfly-bootstrap-combobox/js/bootstrap-combobox.js"></script>
 	<script src="node_modules/underscore/underscore-min.js"></script>
 	<script src="node_modules/clipboard/dist/clipboard.min.js"></script>
-	<script src="extras/sidebar-tab-switcher.js?litbasketsRevision=38"></script>
-	<script src="extras/api.js?litbasketsRevision=38"></script>
-	<script src="extras/index.model.js?litbasketsRevision=38"></script>
-	<script src="extras/index.controller.search.js?litbasketsRevision=38"></script>
-	<script src="extras/index.controller.sources.topbar.js?litbasketsRevision=38"></script>
-	<script src="extras/index.controller.sources.js?litbasketsRevision=38"></script>
-	<script src="extras/index.controller.filters.js?litbasketsRevision=38"></script>
-	<script src="extras/index.controller.js?litbasketsRevision=38"></script>
+	<script src="extras/sidebar-tab-switcher.js?litbasketsRevision=39"></script>
+	<script src="extras/api.js?litbasketsRevision=39"></script>
+	<script src="extras/index.model.js?litbasketsRevision=39"></script>
+	<script src="extras/index.controller.search.js?litbasketsRevision=39"></script>
+	<script src="extras/index.controller.sources.topbar.js?litbasketsRevision=39"></script>
+	<script src="extras/index.controller.sources.js?litbasketsRevision=39"></script>
+	<script src="extras/index.controller.filters.js?litbasketsRevision=39"></script>
+	<script src="extras/index.controller.js?litbasketsRevision=39"></script>
 
 	
 	<title>Litbaskets</title>
