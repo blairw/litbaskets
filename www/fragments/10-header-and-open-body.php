c9a038090149f8ff1f5844f9e48e1b9976b9b30f
Blair Wang <hello@blair.wang>
2019-10-21 01:15:55 +1100

    [chore] litbasketsRevision=43

--- www/fragments/10-header-and-open-body.php
+++ www/fragments/10-header-and-open-body.php
@@ -2,7 +2,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="<?php echo $server ?>/node_modules/patternfly/dist/css/patternfly.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo $server ?>/node_modules/patternfly/dist/css/patternfly-additions.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo $server ?>/extras/styles.css?litbasketsRevision=42" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $server ?>/extras/styles.css?litbasketsRevision=43" />
 	
 	<script src="<?php echo $server ?>/node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="<?php echo $server ?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -12,16 +12,16 @@
 	<script src="<?php echo $server ?>/node_modules/patternfly-bootstrap-combobox/js/bootstrap-combobox.js"></script>
 	<script src="<?php echo $server ?>/node_modules/underscore/underscore-min.js"></script>
 	<script src="<?php echo $server ?>/node_modules/clipboard/dist/clipboard.min.js"></script>
-	<script src="<?php echo $server ?>/extras/sidebar-tab-switcher.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/api.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.model.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.externalLogic.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.controller.initial.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.controller.search.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.controller.sources.topbar.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.controller.sources.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.controller.filters.js?litbasketsRevision=42"></script>
-	<script src="<?php echo $server ?>/extras/index.controller.js?litbasketsRevision=42"></script>
+	<script src="<?php echo $server ?>/extras/sidebar-tab-switcher.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/api.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.model.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.externalLogic.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.controller.initial.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.controller.search.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.controller.sources.topbar.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.controller.sources.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.controller.filters.js?litbasketsRevision=43"></script>
+	<script src="<?php echo $server ?>/extras/index.controller.js?litbasketsRevision=43"></script>
 
 	
 	<title>Litbaskets</title>
