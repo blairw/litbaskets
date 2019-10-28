b93406d63c0f1e37a702ae3dc359e68008ea1e13
Blair Wang <hello@blair.wang>
2019-10-29 09:27:42 +1100

    [feat] Copy changes FAQ

--- www/fragments/50-litbaskets-about.php
+++ www/fragments/50-litbaskets-about.php
@@ -1,5 +1,44 @@
 <div id="litbaskets-about" style="display: none; width: 90%; max-width: 90rem; margin: 0 auto; padding: 1rem;">
 	<h1>FAQ</h1>
+
+	<h2>
+		Do I need Scopus to use Litbaskets.io?
+	</h2>
+	<p class="litfam_faq_para">
+		You will need to have access to Scopus to actually use the search as litbaskets.io uses Scopus to search through content published in IS journals.
+		Many universities and research institutes have access to Scopus.
+		If you are unsure you can check access using the search function at <a href="https://www.scopus.com/checkaccess.uri">https://www.scopus.com/checkaccess.uri</a>.
+		If you don't have access, possibly one of your collaborators has.
+	</p>
+
+	<h2>
+		How does Litbaskets.io work?
+	</h2>
+	<p class="litfam_faq_para">
+		We have combined multiple listings of IS journals into a master list containing several hundreds of journals. You can see the journals for each list in the 'sources' tab. We then rank journals according to how often they appear across all journal lists to create different 'litbaskets'. If you want to know more about how Litbaskets.io works please read our paper.
+	</p>
+
+	<h2>
+		Can I refine my search?
+	</h2>
+	<p class="litfam_faq_para">
+		Yes, Scopus provides different ways for refining search results that all can be used. Use Litbaskets.io as a starting point to generate a search string to start your search in Scopus, then use the 'refine search' function in Scopus for a successive fractions strategy. More details on different search strategies for improving literature searches are provided in the appendix of this CAIS article: <a href="https://aisel.aisnet.org/cais/vol34/iss1/12/">https://aisel.aisnet.org/cais/vol34/iss1/12/</a>.
+	</p>
+
+	<h2>
+		I have access to Scopus but when I click search I only get a window with a search string?
+	</h2>
+	<p class="litfam_faq_para">
+		If you select more than ~52 journals the predictable URL for your search is capped. In other words your search will fail. Of courses you can search more than 52 journals but this only works by copy and pasting the search string from Litbaskets.io into the 'advanced search' in Scopus. After you have done this once you will see this is really easy and quickly achieved
+	</p>
+
+	<h2>
+		Can I see what journals litbaskets.io is searching though?
+	</h2>
+	<p class="litfam_faq_para">
+		Yes using the 'customize search' function you can display a list of the journals included in any of our litbaskets. You can also customize your search by including or removing additional journals for your search. Please note that a direct search in Scopus will only work with up to 52 journals if you search through more journals you will need to copy the search string generated for you into the advanced search field in Scopus.
+	</p>
+	
 	<h2>You're missing an important journal, can I make a suggestion?</h2>
 	<p class="litfam_faq_para">
 		Yes you can! please use our
