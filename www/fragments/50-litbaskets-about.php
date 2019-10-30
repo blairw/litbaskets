a700333d37cd1aa22e6eeb504a7d63605469ce43
Blair Wang <hello@blair.wang>
2019-10-30 15:35:21 +1100

    [chore] copy changes; yarn upgrade

--- www/fragments/50-litbaskets-about.php
+++ www/fragments/50-litbaskets-about.php
@@ -15,7 +15,12 @@
 		How does Litbaskets.io work?
 	</h2>
 	<p class="litfam_faq_para">
-		We have combined multiple listings of IS journals into a master list containing several hundreds of journals. You can see the journals for each list in the 'sources' tab. We then rank journals according to how often they appear across all journal lists to create different 'litbaskets'. If you want to know more about how Litbaskets.io works please read our paper.
+		We have combined multiple listings of IS journals into a master list containing several hundreds of journals. You can see the journals for each list in the 'sources' tab. We then rank journals according to how often they appear across all journal lists to create different 'litbaskets'.
+		If you want to know more about how Litbaskets works or if you want to refer to Litbaskets in your
+		research, please have a look at our paper:
+		<em>
+		<a href="https://www.researchgate.net/publication/336868520_wwwlitbasketsio_an_IT_Artifact_Supporting_Exploratory_Literature_Searches_for_Information_Systems_Research">
+		www.litbaskets.io, an IT Artifact Supporting Exploratory Literature Searches for Information Systems Research</a></em>.
 	</p>
 
 	<h2>
@@ -52,34 +57,5 @@
 		and
 		<a href="https://blair.wang/">Blair Wang</a>.
 		Illustration by <a href="https://www.instagram.com/acidyellows/" target="_blank">acidyellows</a>.
-	</p>
-	<h2>How did you select which journals to include?</h2>
-	<p class="litfam_faq_para">
-		This was based on a collective aggregation of lists provided in the following articles:
-	</p>
-	<br />
-	<p class="litfam_bibliography_entry">
-		Association for Information Systems (AIS), 2011, <em>Senior Scholars' Basket of Journals</em>, <a href="https://aisnet.org/general/custom.asp?page=SeniorScholarBasket" target="_blank">https://aisnet.org/general/custom.asp?page=SeniorScholarBasket</a>
-	</p>
-	<p class="litfam_bibliography_entry">
-		Australian Business Deans Council (ABDC), 2016, <em>ABDC Journal Quality List</em>, <a href="https://abdc.edu.au/research/abdc-journal-list/" target="_blank">https://abdc.edu.au/research/abdc-journal-list/</a>
-	</p>
-	<p class="litfam_bibliography_entry">
-		Chan H.C., Guness V., Kim H.-W., 2015, 'A method for identifying journals in a discipline: An application to information systems', <em>Information and Management</em>, vol. 52, no. 2, doi: <a href="https://dx.doi.org/10.1016/j.im.2014.11.003" target="_blank">10.1016/j.im.2014.11.003</a>
-	</p>
-	<p class="litfam_bibliography_entry">
-		Chartered Association of Business Schools (CABS), 2018, <em>Academic Journal Guide</em>, <a href="https://charteredabs.org/academic-journal-guide-2018/" target="_blank">https://charteredabs.org/academic-journal-guide-2018/</a>
-	</p>
-	<p class="litfam_bibliography_entry">
-		Holsapple C.W., 2009, 'A new map for Knowledge dissemination channels', <em>Communications of the ACM</em>, vol. 52, no. 3, doi: <a href="https://dx.doi.org/10.1145/1467247.1467276" target="_blank">10.1145/1467247.1467276</a>
-	</p>
-	<p class="litfam_bibliography_entry">
-		Lamp, J. W. 2004. Index of Information Systems Journals. Geelong, Deakin University. [Online] Available: http://lamp.infosys.deakin.edu.au/journals/
-	</p>
-	<p class="litfam_bibliography_entry">
-		Stewart A., Cotton J.L., & Adya M., 2017, 'Information Systems: A House Divided?', <em>Communications of the Association for Information Systems</em>, vol. 41, doi: <a href="https://dx.doi.org/10.17705/1CAIS.04124" target="_blank">10.17705/1CAIS.04124</a>
-	</p>
-	<p class="litfam_bibliography_entry">
-		Wilcocks L., Whitley E.A., & Avgerou C., 2008, 'The ranking of top IS journals: a perspective from the London School of Economics', <em>European Journal of Information Systems</em>, vol. 17, no. 2, doi: <a href="https://dx.doi.org/10.1057/ejis.2008.9" target="_blank">10.1057/ejis.2008.9</a>
 	</p>
 </div>
