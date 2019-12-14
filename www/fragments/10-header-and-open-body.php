0a2a5154b2ebb61ecb6a594917d8c5fc7389269c
Blair Wang <hello@blair.wang>
2019-12-14 12:27:56 +0100

    [chore] litbasketsRevision=59

--- www/fragments/10-header-and-open-body.php
+++ www/fragments/10-header-and-open-body.php
@@ -2,7 +2,7 @@
 	
 	<link rel="stylesheet" type="text/css" href="<?php echo $WWW_SERVER ?>node_modules/patternfly/dist/css/patternfly.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo $WWW_SERVER ?>node_modules/patternfly/dist/css/patternfly-additions.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo $WWW_SERVER ?>extras/styles.css?litbasketsRevision=58" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $WWW_SERVER ?>extras/styles.css?litbasketsRevision=59" />
 	
 	<script src="<?php echo $WWW_SERVER ?>node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="<?php echo $WWW_SERVER ?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -14,14 +14,14 @@
 	<script src="<?php echo $WWW_SERVER ?>node_modules/clipboard/dist/clipboard.min.js"></script>
 	<script src="<?php echo $WWW_SERVER ?>node_modules/downloadjs/download.min.js"></script>
 	<script src="<?php echo $WWW_SERVER ?>node_modules/nvl/dist/index.js"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/sidebar-tab-switcher.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.model.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.externalLogic.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.search.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.sources.topbar.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.sources.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.filters.js?litbasketsRevision=58"></script>
-	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.js?litbasketsRevision=58"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/sidebar-tab-switcher.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.model.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.externalLogic.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.search.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.sources.topbar.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.sources.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.filters.js?litbasketsRevision=59"></script>
+	<script src="<?php echo $WWW_SERVER ?>extras/index.controller.js?litbasketsRevision=59"></script>
 	<script>
 		var API_ROOT = "<?php echo $API_SERVER ?>";
 	</script>
