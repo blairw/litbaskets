cbe782454e845e3b6f43e42be0ebbffbf5d48e5e
Blair Wang <hello@blair.wang>
2020-12-28 14:46:49 +1100

    [chore] upgrade yarn

--- sync/push-to-production.sh
+++ sync/push-to-production.sh
@@ -1,5 +1,1 @@
-rsync -avz -e "ssh -p2683" \
-    --no-perms --no-owner --no-group \
-    --exclude "/cgi-bin" \
-    --exclude "/www/api.js" \
-    --delete --progress ./www/ litbaske@litbaskets.io:/home2/litbaske/public_html
+rclone sync -v --transfers 1 --checkers 1 ./www/ litbaskets://public_html
