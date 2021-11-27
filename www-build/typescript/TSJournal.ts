67cb66f2557ea0fa9fc1c570284ddfa6c694fc32
Blair <hello@blair.wang>
2021-11-27 15:02:45 +1100

    [refactor] Rename variables in `TSJournal.ts`

--- www-build/typescript/TSJournal.ts
+++ www-build/typescript/TSJournal.ts
@@ -1,9 +1,9 @@
 class TSJournal {
-	journalId: string;
-	journalName: string;
+	journal_id: string;
+	journal_name: string;
 	url: string;
 	issn: string;
 	issne: string;
-	scopusSourceId: string;
-	scopusCoverage: string;
+	scopus_sourceid: string;
+	scopus_coverage: string;
 }
